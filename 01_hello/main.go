5d62f68818d2ab200fc611b0ce12a7c55fd89709
Brendon Dulam <brendondulam06@gmail.com>
2019-10-16 06:03:55 +0900

    initial commit

---
+++ 01_hello/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	name, age := "Brendon", 15
+
+	fmt.Println(name, age)
+}
---
+++ 02_packages/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Floor(1.2))
+	fmt.Println(math.Sqrt(1.2))
+}

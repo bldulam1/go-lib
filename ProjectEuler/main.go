90a79456d1349773ab95ddbc322099b326e489f1
Brendon Dulam <brendondulam06@gmail.com>
2019-10-20 17:14:38 +0900

    12. Highly divisible triangular number

--- ProjectEuler/main.go
+++ ProjectEuler/main.go
@@ -1,6 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	product := largestProductInGrid("./files/prob11.txt")
-	println(product)
+	divisorsLen := 0
+	index := 12370
+	maxDivisorsLen := 500
+	for divisorsLen <= maxDivisorsLen {
+		an := arithmeticSum(index, 1, 1)
+		divisors := getDivisors(an)
+		divisorsLen = len(divisors)
+		fmt.Println(index, an, divisorsLen)
+		index++
+	}
 }
--- ProjectEuler/prob12.go
+++ ProjectEuler/prob12.go
@@ -1,5 +1,28 @@
 package main
 
 func arithmeticSum(n int, a1 int, increment int) int {
-	return (n / 2) * (2*a1 + (n-1)*increment)
+	return (2*a1 + (n-1)*increment) * n / 2
+}
+
+func getDivisors(n int) []int {
+	divisors := make(map[int]int)
+	divisors[1] = n
+
+	factors := make([]int, 0)
+
+	if n > 1 {
+		factors = append(factors, 1)
+		factors = append(factors, n)
+		for i := 2; i < n/2; i++ {
+			if n%i == 0 && divisors[n/i] == 0 {
+				divisors[i] = n / i
+				factors = append(factors, i)
+				if divisors[i] != i {
+					factors = append(factors, divisors[i])
+				}
+			}
+		}
+	}
+
+	return factors
 }
